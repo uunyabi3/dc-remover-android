7ce8308c5d68582970d2830c75014fe807169efb
uunyabi3 <nyabi021@gmail.com>
2025-12-27 13:05:07 +0900

    feat: Add initial Android application build configuration with Compose, Hilt, and network dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.nyabi.dcremover"
         minSdk = 33
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         vectorDrawables {
             useSupportLibrary = true
